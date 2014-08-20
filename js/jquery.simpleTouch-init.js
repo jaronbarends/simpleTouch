d7030afa8ae9b97fde5ebbd9f3ad65d4421abb61
jaronbarends <ik@jaron.nl>
2014-08-20 16:46:29 +0200

    added move event; code cleanup
    added a move event; added fs prefix for file-scope "globals", replaced
    double quotes with single ones, etc

--- js/jquery.simpleTouch-init.js
+++ js/jquery.simpleTouch-init.js
@@ -1,41 +1,29 @@
 (function($) {
 	
 	//cached elements
-	var $logpane;
+	var $logpane,
+		pluginName = 'simpleTouch';
 	
 	//-- Start handlers --
+		
+		function touchStatusHandler(evt, data) {
+			console.log('touchStatusChange - phase: '+data.phase+'; dir: '+data.direction+'; dist: ('+data.distance.x+', '+data.distance.y+')');
+		}
+		
+		function moveHandler(evt, data) {
+			console.log('move '+data.direction+'; dist: ('+data.distance.x+', '+data.distance.y+');');
+		}
+		
 		function swipeHandler(evt, data) {
-			log('swipe '+data.direction+'; dist: ('+data.distance.x+', '+data.distance.y+');');
-			data.lastEvent = 'swipe';
-			logTouchEvent(data);
+			console.log('swipe '+data.direction+'; dist: ('+data.distance.x+', '+data.distance.y+');');
 		}
 		
 		function swipeMoveHandler(evt, data) {
-			log('swipemove: '+data.direction+'; dist: ('+data.distance.x+', '+data.distance.y+')');
-			data.lastEvent = 'swipeMove';
-			logTouchEvent(data);
+			console.log('swipemove: '+data.direction+'; dist: ('+data.distance.x+', '+data.distance.y+')');
 		}
-		
-		function touchStatusHandler(evt, data) {
-			log('Status - phase: '+data.phase+'; dir: '+data.direction+'; dist: ('+data.distance.x+', '+data.distance.y+')');
-			data.lastEvent = 'touchStatus';
-			logTouchEvent(data);
-		}
+
 	//-- End handlers --
-	
-	//-- Start logging functions --
-		function logTouchEvent(data) {
-			$('#lasteventlog').text(data.lastEvent);
-			$('#phaselog').text(data.phase);
-			
-			//log touch data
-			var prf = 't0-';
-			$('#'+prf+'x').text(data.end.x);
-			$('#'+prf+'y').text(data.end.y);
-			$('#'+prf+'dir').text(data.direction);
-			$('#'+prf+'dist').text(data.distance.x+', '+data.distance.y);
-		}
-	//-- End logging functions --
+
 	
 	//-- Start init functions --
 		function initTouchTest() {
@@ -48,49 +36,15 @@
 			}
 
 			$('#toucharea').simpleTouch(options)
-				//.on('touchStatus.touchEvent', touchStatusHandler)
-				//.on('swipeMove.touchEvent', swipeMoveHandler)
-				.on('swipe.touchEvent', swipeHandler);
+				.on('touchStatusChange.'+pluginName, touchStatusHandler)
+				.on('move.'+pluginName, moveHandler)
+				.on('swipe.'+pluginName, swipeHandler)
+				.on('swipeMove.'+pluginName, swipeMoveHandler);
 		}
 		
-		function initVars() {
-			$logpane = $('#log');
-		}
 	//-- End init functions --
 		
-	//-- Start helper functions --
-		function initClear() {
-			$('#clear').click(function(evt) {
-				evt.preventDefault();
-				$logpane.html('');
-				//clear logs
-				clearElm('lasteventlog');
-				clearElm('phaselog');
-				clearTouches();
-			});
-		}
-		
-		function clearTouches() {
-			prf = 't0-';
-			clearElm(prf+'x');
-			clearElm(prf+'y');
-			clearElm(prf+'dir');
-			clearElm(prf+'dist');
-		}
-		
-		function clearElm(id) {
-			$('#'+id).html('');
-		}
-	
-		function log(msg) {
-			var t = $logpane.html();
-			$logpane.html(msg+'<br>'+t);
-		}
-	//-- End helper functions --
-	
 	function init() {
-		initVars();
-		initClear();
 		initTouchTest();
 	}
 	
--- js/jquery.simpleTouch.js
+++ js/jquery.simpleTouch.js
@@ -10,52 +10,53 @@
 		if (!this) return false;
 		
 		// Default thresholds & swipe functions
-		var defaults = {
+		var fsDefaults = {
 					
 			swipeThreshold: 75,// int - The number of pixels that the user must move their finger by before it is considered a swipe
-			mouseEmulation: false,//Boolean, if true, when the browser doesn't support touch, the mouse can be used for swiping
+			mouseEmulation: true,//Boolean, if true, when the browser doesn't support touch, the mouse can be used for swiping
 			triggerOnTouchEnd: true,// Boolean, if true, the swipe events are triggered when the touching ends is received (user releases all fingers).  If false, it will be triggered on reaching the swipeThreshold
-			allowPageScroll: "auto" /* How the browser handles page scrolls when the user is swiping on a touchSwipe object. 
-				"auto" : all undefined swipes will cause the page to scroll in that direction.
- 				"none" : the page will not scroll when user swipes.
- 				"horizontal" : will force page to scroll on horizontal swipes.
- 				"vertical" : will force page to scroll on vertical swipes.
- 				TODO: IK MIS HIER "always"
+			allowPageScroll: 'auto' /* How the browser handles page scrolls when the user is swiping on a touchSwipe object. 
+				'auto' : all undefined swipes will cause the page to scroll in that direction.
+ 				'none' : the page will not scroll when user swipes.
+ 				'horizontal' : will force page to scroll on horizontal swipes.
+ 				'vertical' : will force page to scroll on vertical swipes.
 			*/
 		};
 		
 		//Constants
-		var LEFT = "left",
-			RIGHT = "right",
-			UP = "up",
-			DOWN = "down",
+		var fsC = {
+			LEFT: 'left',
+			RIGHT: 'right',
+			UP: 'up',
+			DOWN: 'down',
 		
-			NONE = "none",
-			HORIZONTAL = "horizontal",
-			VERTICAL = "vertical",
-			AUTO = "auto",
+			NONE: 'none',
+			HORIZONTAL: 'horizontal',
+			VERTICAL: 'vertical',
+			AUTO: 'auto',
 		
-			PHASE_START_MOVE = "startMove",
-			PHASE_MOVE = "move",
-			PHASE_SWIPE = "swipe",
-			PHASE_END = "end",
-			PHASE_CANCEL = "cancel";
-		
-		// vars for page scrolling
-		var allowScrollHor = true,
-			allowScrollVert = true;
+			PHASE_START_MOVE: 'startMove',
+			PHASE_MOVE: 'move',
+			PHASE_SWIPE: 'swipe',
+			PHASE_END: 'end',
+			PHASE_CANCEL: 'cancel',
+
+			EVENT_TOUCH_STATUS_CHANGE: 'touchStatusChange',
+			EVENT_MOVE: 'move',
+			EVENT_SWIPE_MOVE: 'swipeMove'
+		};
 		
 		// vars vor keeping track of events statuses
-		var swipeEventFired = false,
-			touchEnded = true;
+		var fsSwipeEventFired = false,
+			fsTouchEnded = true;
 			
 		//object to pass data around
-		var data = {};
+		var fsData = {};
 		
 		//-- let's get started :) --
 		
 		if (options) {
-			$.extend(defaults, options);
+			$.extend(fsDefaults, options);
 		}
 		
 		return this.each(function() {
@@ -64,17 +65,17 @@
 			
 				function touchStartHandler(evt) {
 
-					if (touchEnded) {
+					if (fsTouchEnded) {
 						//then it's really the start of a new touch
-						touchEnded = false;
+						fsTouchEnded = false;
 						initEvtVars();
-						setPhase(PHASE_START_MOVE);
+						setPhase(fsC.PHASE_START_MOVE);
 					} else {
 						//a finger was added to an existing touch
 					}
 			
 					var touch = evt.touches[0];
-					data.start = getTouchPos(touch);
+					fsData.start = getTouchPos(touch);
 					
 					//now process the other event variables
 					setEventData(evt);
@@ -90,19 +91,20 @@
 					handleDefaultScroll(evt);
 					
 					//If we trigger whilst dragging, not on touch end, then calculate now...
-					if (!defaults.triggerOnTouchEnd && distanceHasPassedThreshold()) {
+					if (!fsDefaults.triggerOnTouchEnd && distanceHasPassedThreshold()) {
 						// if the user swiped more than the minimum length, perform the appropriate action
 						//2 possibilities: if swipeEvents haven't been fired yet, fire swipeHandlers; otherwise trigger swipeMove
-						setPhase(PHASE_SWIPE);
+						setPhase(fsC.PHASE_SWIPE);
 						
-						if (!swipeEventFired) {
+						if (!fsSwipeEventFired) {
 							triggerSwipeHandlers(evt);
 						} else {
-							triggerTouchEvent('swipeMove');
+							triggerTouchEvent(fsC.EVENT_SWIPE_MOVE);
 						}
 					} else {
-						setPhase(PHASE_MOVE);
+						setPhase(fsC.PHASE_MOVE);
 					}
+					triggerTouchEvent(fsC.EVENT_MOVE);
 
 					triggerStatusHandler(evt);
 				}
@@ -114,11 +116,11 @@
 					if (!evt.touches.length) {
 						//it's really the end of this touch, and not just one finger being lifted
 						//we must not update the event data, because we want it to contain the last know position
-						setPhase(PHASE_END);
+						setPhase(fsC.PHASE_END);
 						
 						if (!distanceHasPassedThreshold()) {
-							setPhase(PHASE_CANCEL);
-						} else if (defaults.triggerOnTouchEnd) {
+							setPhase(fsC.PHASE_CANCEL);
+						} else if (fsDefaults.triggerOnTouchEnd) {
 							triggerSwipeHandlers(evt);
 						}
 						
@@ -127,69 +129,70 @@
 				}
 			
 				function triggerStatusHandler(evt) {
-					//update status
-					if (data.phase != data.prevPhase) {
-						triggerTouchEvent('touchStatus');
+					//update status and send statusChange event when status changed
+					if (fsData.phase != fsData.prevPhase) {
+						triggerTouchEvent(fsC.EVENT_TOUCH_STATUS_CHANGE);
 					}
 				}
 			
 			/*-- End handlers -- --*/
 			
 			
-			/*-- Start data object functions --*/
+			/*-- Start fsData object functions --*/
 
 				function initEvtVars() {
-					swipeEventFired = false;
-					data = {};
+					fsSwipeEventFired = false;
+					fsData = {};
 				}
 				
 				function setEventData(evt) {
 					//processes all relevant data for an event
 					var touch = evt.touches[0];
 
-					//data.start has been set in touchStartHandler; make sure not to override it here
+					//fsData.start has been set in touchStartHandler; make sure not to override it here
 					//because this function gets called upon every event
-					data.end = getTouchPos(touch);
-					data.distance = getDistanceObj(data.start, data.end);
-					data.direction = getDirection(data.start, data.end);
+					fsData.end = getTouchPos(touch);
+					fsData.distance = getDistanceObj(fsData.start, fsData.end);
+					fsData.direction = getDirection(fsData.start, fsData.end);
 
-					//now add general evt vars to data object
-					data.originalEvent = evt;
+					//now add general evt vars to fsData object
+					fsData.originalEvent = evt;
 				}
 				
 				function setPhase(phs) {
 					//sets the new phase
-					data.prevPhase = data.phase;
-					data.phase = phs;
-					if (phs === PHASE_CANCEL || phs === PHASE_END) {
-						touchEnded = true;
+					fsData.prevPhase = fsData.phase;
+					fsData.phase = phs;
+					if (phs === fsC.PHASE_CANCEL || phs === fsC.PHASE_END) {
+						fsTouchEnded = true;
 					}
 				}
 			
-			/*-- End data object functions --*/
+			/*-- End fsData object functions --*/
+
 			
 			/*-- Start trigger functions --*/
 			
 				function triggerTouchEvent(evtName) {
 					//suffix evtName with namespace
-					evtName += '.touchEvent';
-					$this.trigger(evtName, data);
+					evtName += '.'+pluginName;
+					$this.trigger(evtName, fsData);
 				}
 				
 				function triggerSwipeHandlers(evt) {
 					//trigger all appropriate swipeHandlers
-					swipeEventFired = true;
+					fsSwipeEventFired = true;
 					
 					//trigger catch all event handler
 					triggerTouchEvent('swipe');
 					
 					//trigger direction specific event handlers
-					var direction = data.direction,
+					var direction = fsData.direction,
 						evts = [];//TODO: kan deze niet maar 1x gedefinieerd worden?
-						evts[LEFT] = 'swipeLeft';
-						evts[RIGHT] = 'swipeRight';
-						evts[UP] = 'swipeUp';
-						evts[DOWN] = 'swipeDown';
+						evts[fsC.LEFT] = 'swipeLeft';
+						evts[fsC.RIGHT] = 'swipeRight';
+						evts[fsC.UP] = 'swipeUp';
+						evts[fsC.DOWN] = 'swipeDown';
 						triggerTouchEvent(evts[direction]);
 				}
 				
@@ -234,32 +237,32 @@
 			 * This will essentially allow page scrolling or not when the user is swiping on a touchSwipe object.
 			 */
 			function handleDefaultScroll(evt) {
-				if( defaults.allowPageScroll == NONE ) {
+				if( fsDefaults.allowPageScroll == fsC.NONE ) {
 					evt.preventDefault();
 				} else {
-					var auto = (defaults.allowPageScroll == AUTO);
+					var auto = (fsDefaults.allowPageScroll == fsC.AUTO);
 					
-					switch(data.direction) {
-						case LEFT :
-							if ( (defaults.swipeLeft && auto) || (!auto && defaults.allowPageScroll!=HORIZONTAL)) {
+					switch(fsData.direction) {
+						case fsC.LEFT :
+							if ( (fsDefaults.swipeLeft && auto) || (!auto && fsDefaults.allowPageScroll!=fsC.HORIZONTAL)) {
 								evt.preventDefault();
 							}
 							break;
 						
-						case RIGHT :
-							if ( (defaults.swipeRight && auto) || (!auto && defaults.allowPageScroll!=HORIZONTAL)) {
+						case fsC.RIGHT :
+							if ( (fsDefaults.swipeRight && auto) || (!auto && fsDefaults.allowPageScroll!=fsC.HORIZONTAL)) {
 								evt.preventDefault();
 							}
 							break;
 
-						case UP :
-							if ( (defaults.swipeUp && auto) || (!auto && defaults.allowPageScroll!=VERTICAL)) {
+						case fsC.UP :
+							if ( (fsDefaults.swipeUp && auto) || (!auto && fsDefaults.allowPageScroll!=fsC.VERTICAL)) {
 								evt.preventDefault();
 							}
 							break;
 						
-						case DOWN :	
-							if ( (defaults.swipeDown && auto) || (!auto && defaults.allowPageScroll!=VERTICAL)) {
+						case fsC.DOWN :	
+							if ( (fsDefaults.swipeDown && auto) || (!auto && fsDefaults.allowPageScroll!=fsC.VERTICAL)) {
 								evt.preventDefault();
 							}
 							break;
@@ -284,9 +287,9 @@
 					var dy = end.y - start.y;
 					if (Math.abs(dx) >= Math.abs(dy)) {
 						//we're moving horizontally
-						return ( (dx > 0) ? RIGHT : LEFT);
+						return ( (dx > 0) ? fsC.RIGHT : fsC.LEFT);
 					} else {
-						return ( (dy > 0) ? DOWN : UP);
+						return ( (dy > 0) ? fsC.DOWN : fsC.UP);
 					}
 				}
 				
@@ -301,29 +304,31 @@
 				}
 				
 				function distanceHasPassedThreshold() {
-					var distance = data.distance,
+					var distance = fsData.distance,
 						dx = Math.abs(distance.x),
 						dy = Math.abs(distance.y);
 
-					return (Math.max(dx,dy) >= defaults.swipeThreshold);
+					return (Math.max(dx,dy) >= fsDefaults.swipeThreshold);
 				}
 				
 			/*-- End helper functions --*/
 			
+
 			/*-- Start initialization --*/
+
 			var $this = $(this);
 			if ('ontouchstart' in window) {
 				try {
-					this.addEventListener("touchstart", touchStartHandler, false);
-					this.addEventListener("touchmove", touchMoveHandler, false);
-					this.addEventListener("touchend", touchEndHandler, false);
+					this.addEventListener('touchstart', touchStartHandler, false);
+					this.addEventListener('touchmove', touchMoveHandler, false);
+					this.addEventListener('touchend', touchEndHandler, false);
 				} catch(e) {
 					//touch not supported
 				}
 			} else {
 				//bind stuff to mouse events
-				$this.on("mousedown", mousedownHandler);
-				$this.on("mouseup", mouseupHandler);
+				$this.on('mousedown', mousedownHandler);
+				$this.on('mouseup', mouseupHandler);
 			}
 				
 		});
